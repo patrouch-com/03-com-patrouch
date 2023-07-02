d56985b0eb25c75cd5af8ae46fb4252fca8a1afa
Chef <cheftech@patraldo.dev>
2023-07-01 22:31:03 -0600

    troubleshooting sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<h1>Welcome to Patrouch.com </h1>
+<h1>Welcome to Patrouch.com</h1>
+<p>this is the home page.</p>
+
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
-<h1>home</h1>
-<p>this is the home page.</p>
+<h1>about</h1>
+<p>this is the about page.</p>
 
