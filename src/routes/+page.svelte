7e5237c7c48aea7e0f4df6107706df81582cdca6
Chef <cheftech@patraldo.dev>
2023-07-02 22:19:20 -0600

    Adding  alias to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+	import { message } from '$lib/message.js';
 	export let data;
+
 </script>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
 
 <h1>Welcome to Patrouch.com</h1>
 
+<p>{message}</p>
 
