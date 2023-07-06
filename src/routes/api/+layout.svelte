e8e2933a1feb3b4af877f3b2053ab146a36d3d61
Chef <cheftech@patraldo.dev>
2023-07-05 21:05:30 -0600

    add nested nav for api

---
+++ src/routes/api/+layout.svelte
@@ -0,0 +1,33 @@
+<!-- src/routes/api/random-number/-->
+
+
+<div class="submenu">
+<ul>
+<li><a href="/api/random-number/">Random Number</a>
+</li>
+</ul>
+</div>
+
+<slot></slot>
+
+<style>
+	ul {
+		display: flex;
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+	li {
+		margin-right: 20px;
+		font-weight: bold;
+	}
+	a {
+		color:sienna;
+		text-decoration: none;
+	}
+	a:hover {
+		color:saddlebrown;
+	}
+
+
+</style>
