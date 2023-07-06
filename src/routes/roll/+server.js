97a80162fe2c594ed0d0a7a8d2610a262f246b2f
Chef <cheftech@patraldo.dev>
2023-07-05 19:55:58 -0600

    added directory src/routes/roll/+page.svelte

--- src/routes/roll/+server.js
+++ src/routes/roll/+server.js
@@ -1,16 +1,8 @@
-<script>
-	/** @type {number} */
-	let number;
+import { json } from '@sveltejs/kit';
 
-	async function roll() {
-		const response = await fetch('/roll');
-		number = await response.json();
-	}
-</script>
+export function GET() {
+	const number = Math.floor(Math.random() * 6) + 1;
 
-<button on:click={roll}>Roll the dice</button>
-
-{#if number !== undefined}
-	<p>You rolled a {number}</p>
-{/if}
+	return json(number);
+}
 
