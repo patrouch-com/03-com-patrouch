33b2a07d8d3c20c0edbe60669168c4eb75fd6fc7
Chef <cheftech@patraldo.dev>
2023-07-05 19:59:46 -0600

    add new routes to nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	<a href="/">home</a>
 	<a href="/about">about</a>
         <a href="/blog">blog</a>
+        <a href="/roll">roll</a>
+        <a href="/add">add</a>
+        <a href="/api">api</a>
+
 </nav>
 
 <slot />
