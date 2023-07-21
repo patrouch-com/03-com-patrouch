c4a753807454acc1ffece4108273c1b9d7189fbc
Chef <cheftech@patraldo.dev>
2023-07-21 17:42:54 -0600

    added +page.svelte to display results

---
+++ src/routes/api/users/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+ export let data;
+ const gophers  = data.gophers;
+ const Component = data.Component;
+
+</script>
+
+
+<h1>{data.title}</h1>
+{#each gophers as gopher}
+<div>
+<Component {gopher} />
+</div>
+{/each}
+
+
