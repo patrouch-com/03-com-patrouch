8160eacf51b385cd292082a1b2856ecf592ca971
Chef Tech <cheftech@patraldo.com>
2024-01-29 13:27:29 -0600

    add video.js media player

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,18 @@
 <script>
-        import videojs from 'video.js';
-	import { onMount } from 'svelte';
+import 'vidstack/player/styles/default/theme.css';
+import 'vidstack/player/styles/default/layouts/video.css';
+import 'vidstack/player';
+import 'vidstack/player/layouts';
+import 'vidstack/player/ui';
 	import { message } from '$lib/message.js';
 	export let data;
 
-  const options = {};
-  let video_element;
-
-  onMount(() => {
-    const player = videojs('vid1', video_element, options, function onPlayerReady() {
-      videojs.log('Your player is ready!');
-
-      // In this context, `this` is the player that was created by Video.js.
-      this.play();
-
-    // How about an event listener?
-      this.on('ended', function() {
-       videojs.log('Awww...over so soon?!');
-      });
-    });
-  });
 </script>
 
-<video id="vid1" class="video-js">
-  <source src="//vjs.zencdn.net/v/oceans.mp4">
-</video>
+<media-player title="Sprite Fight" src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4">
+  <media-provider></media-provider>
+  <media-video-layout thumbnails="https://image.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/storyboard.vtt"></media-video-layout>
+</media-player>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
 
